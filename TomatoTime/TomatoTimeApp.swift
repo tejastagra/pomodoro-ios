0bbe7b14b5c398233d864c66d800b1d423b79efe
Tejas Tagra <29814366+tejastagra@users.noreply.github.com>
2025-06-15 16:22:17 +1000

    Initial Commit

---
+++ TomatoTime/TomatoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoTimeApp.swift
+//  TomatoTime
+//
+//  Created by Tejas Tagra on 15/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct TomatoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TomatoTimeTests/TomatoTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoTimeTests.swift
+//  TomatoTimeTests
+//
+//  Created by Tejas Tagra on 15/6/2025.
+//
+
+import Testing
+@testable import TomatoTime
+
+struct TomatoTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
